109c97525d1e6a707c840ee309e602bfc9c1e74d
Samir Karimov <pragmatecheduaz@gmail.com>
2024-10-15 14:12:58 +0400

    Initial files

---
+++ assets/js/simple-datatables-az.js
@@ -0,0 +1,25 @@
+/**
+ * Azerbaijani translation for Simple-DataTables
+ */
+SimpleDataTables.setLanguage({
+    "searchPlaceholder": "Axtar...",
+    "emptyTable": "Cədvəldə məlumat yoxdur",
+    "info": "_TOTAL_ qeyddən _START_ - _END_ arası göstərilir",
+    "infoEmpty": "0 qeyddən 0 - 0 arası göstərilir",
+    "infoFiltered": "(_MAX_ qeyddən süzüldü)",
+    "lengthMenu": "_MENU_ qeyd göstər",
+    "loadingRecords": "Yüklənir...",
+    "processing": "İşlənilir...",
+    "search": "Axtar:",
+    "zeroRecords": "Uyğun qeyd tapılmadı",
+    "paginate": {
+        "first": "İlk",
+        "last": "Son",
+        "next": "Növbəti",
+        "previous": "Əvvəlki"
+    },
+    "aria": {
+        "sortAscending": ": sütunu artan sıra ilə çeşidləmək üçün aktivləşdir",
+        "sortDescending": ": sütunu azalan sıra ilə çeşidləmək üçün aktivləşdir"
+    }
+});
